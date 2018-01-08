83365ba68482b599ebaafc4610b5ea4310b0dcdd
Kate Schlunz <schlunzk@gmail.com>
2018-01-08 19:06:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
   for(let i = 0; i < array.length; i++){
     if(i===1){
-      console.log("I am 1 strange loop.")
+      return "I am 1 strange loop."
     } else{
-        console.log(`I am ${array[i]} strange loops.`)
+        return `I am ${array[i]} strange loops.`
     }
 
 
