ca0c15e7aa2b791f0d42de90d176d720554bcd76
Kate Schlunz <schlunzk@gmail.com>
2018-01-08 18:34:34 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+  for(var i = 0; i <26; i++){
+    if(i=1){
+      console.log("I am 1 strange loop.")
+
+    }else{
+      console.log(`I am ${i} strange loops.`)
+    }
+
+  }
+  return array; 
+}
